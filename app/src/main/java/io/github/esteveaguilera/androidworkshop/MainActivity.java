a09676ff76fe2ec6ca59a24194454f69c058a8f1
Esteve Aguilera <esteve@fewlaps.com>
2017-12-11 13:08:03 +0100

    First interaction with views

--- app/src/main/java/io/github/esteveaguilera/androidworkshop/MainActivity.java
+++ app/src/main/java/io/github/esteveaguilera/androidworkshop/MainActivity.java
@@ -2,6 +2,10 @@ package io.github.esteveaguilera.androidworkshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText etUser = (EditText) findViewById(R.id.et_write_your_name);
+        Button butStart = (Button) findViewById(R.id.but_start);
+
+        butStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = etUser.getText().toString();
+                String welcomeMessage = "Welcome to the training " + userName + "!";
+
+                Toast.makeText(MainActivity.this, welcomeMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
